f31662b3b4395225e1a4b65759cc342565153fc7
Eddie Corrigall <eddiecorrigall@gmail.com>
2019-10-22 12:14:36 -0400

    Fix setup for Linux

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,14 +3,27 @@
 set -e
 
 # Bootstrap project
-brew install node
+
+if [ -z "$(command -v node)" ]; then
+    brew install node
+fi
+
+if [ -z "$(command -v serverless)" ]; then
+    npm install -g serverless
+fi
+
+# Install node requirements
 npm install
-npm install -g serverless
-python3 -m venv venv
+
+# Install python requirements
+python3.7 -m venv venv
 . venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --requirement requirements.txt
 
 # Install Redis Server
-brew install redis
-brew upgrade redis
-brew services start redis
+# brew install redis
+# brew upgrade redis
+# brew services start redis
+
