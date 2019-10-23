de8fa825423475aba45e340949472559334aac9c
Eddie Corrigall <eddiecorrigall@gmail.com>
2019-10-23 10:53:08 -0400

    Prompt for TINYURL_SECRET_KEY

--- bin/provision.sh
+++ bin/provision.sh
@@ -5,6 +5,13 @@ set -e
 # Run tests first
 sh bin/test.sh
 
+# Collect information for .env file
+while [ -z "${TINYURL_SECRET_KEY}" ]; do
+    read -s -p "Enter TINYURL_SECRET_KEY: " TINYURL_SECRET_KEY
+    echo
+    export TINYURL_SECRET_KEY="${TINYURL_SECRET_KEY}"
+done
+
 # Provision custom domain mapping
 serverless create_domain "$@"
 
