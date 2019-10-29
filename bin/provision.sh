714bd03900f96154a56b2d96ea90506668d4e999
Eddie Corrigall <eddiecorrigall@gmail.com>
2019-10-29 12:50:33 -0400

    LPM new env var TINYURL_DOMAIN

--- bin/provision.sh
+++ bin/provision.sh
@@ -5,7 +5,12 @@ set -e
 # Run tests first
 sh bin/test.sh
 
-# Collect information for .env file
+# Collect information
+while [ -z "${TINYURL_DOMAIN}" ]; do
+	read -p "Enter TINYURL_DOMAIN: " TINYURL_DOMAIN
+	echo
+	export TINYURL_DOMAIN="${TINYURL_DOMAIN}"
+done
 while [ -z "${TINYURL_SECRET_KEY}" ]; do
     read -s -p "Enter TINYURL_SECRET_KEY: " TINYURL_SECRET_KEY
     echo
